ea0e82c1b451b33ec747d0710661b876a2b9aba8
Syed <syed.murtza@dockyard.com>
2023-06-22 22:35:20 +0500

    Add chart_background modifier

---
+++ lib/live_view_native_swift_ui_charts/modifiers/view_fundamentals/chart_background.ex
@@ -0,0 +1,24 @@
+defmodule LiveViewNativeSwiftUiCharts.Modifiers.ChartBackground do
+  use LiveViewNativePlatform.Modifier
+
+  alias LiveViewNativeSwiftUi.Types.KeyName
+
+  modifier_schema "chart_background" do
+    field :alignment, Ecto.Enum,
+      values: ~w(
+      bottom
+      bottom_leading
+      bottom_trailing
+      center
+      leading
+      leading_last_text_baseline
+      top
+      top_leading
+      top_trailing
+      trailing
+      trailing_first_text_baseline
+    )a
+
+    field :content, KeyName, default: nil
+  end
+end
