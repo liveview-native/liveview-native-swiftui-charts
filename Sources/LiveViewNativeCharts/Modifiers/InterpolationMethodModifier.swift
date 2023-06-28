35b7a11562c7eaed76d05e560cd42458d005b504
Carson Katri <Carson.katri@gmail.com>
2023-06-28 15:36:18 -0400

    Add `interpolation_method` modifier

---
+++ Sources/LiveViewNativeCharts/Modifiers/InterpolationMethodModifier.swift
@@ -0,0 +1,43 @@
+//
+//  OffsetModifier.swift
+//
+//
+//  Created by Carson Katri on 6/28/23.
+//
+
+import Charts
+import LiveViewNative
+
+/// Set the interpolation method for ``LineMark`` and ``AreaMark``.
+///
+/// Customize the shape of area and line marks with different interpolation methods.
+///
+/// ```html
+/// <LineMark modifiers={interpolation_method(:catmull_rom)} />
+/// <AreaMark modifiers={interpolation_method(:cardinal)} />
+///
+/// ## Arguments
+/// * ``x``
+/// * ``y``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+struct InterpolationMethodModifier: ContentModifier {
+    typealias Builder = ChartContentBuilder
+    
+    /// The interpolation method to use.
+    /// 
+    /// See ``LiveViewNativeCharts/Charts/InterpolationMethod`` for a list of possible values.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    let method: InterpolationMethod
+    
+    func apply<R: RootRegistry>(
+        to content: Builder.Content,
+        on element: ElementNode,
+        in context: Builder.Context<R>
+    ) -> Builder.Content {
+        content.interpolationMethod(method)
+    }
+}
