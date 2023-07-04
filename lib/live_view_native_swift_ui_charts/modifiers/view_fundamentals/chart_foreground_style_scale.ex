aa2862b67b630c4a4d145248be13ad5d582381de
Syed <syed.murtza@dockyard.com>
2023-07-04 23:33:19 +0500

    Change alias of ShapeStyle

--- lib/live_view_native_swift_ui_charts/modifiers/view_fundamentals/chart_foreground_style_scale.ex
+++ lib/live_view_native_swift_ui_charts/modifiers/view_fundamentals/chart_foreground_style_scale.ex
@@ -2,6 +2,7 @@ defmodule LiveViewNativeSwiftUiCharts.Modifiers.ChartForegroundStyleScale do
   use LiveViewNativePlatform.Modifier
 
   alias LiveViewNativeSwiftUiCharts.Types.{ScaleDomain, ShapeStyle, BasicChartSymbolShape, ScaleType}
+  alias LiveViewNativeSwiftUi.Types.ShapeStyle
 
   modifier_schema "chart_symbol_scale" do
     field :mapping, {:map, ShapeStyle}
