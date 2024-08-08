be9c96355682ca25467c0f3900e5640766b4d02a
Carson Katri <Carson.katri@gmail.com>
2024-08-08 15:47:58 -0400

    Switch to `main` branch

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["LiveViewNativeCharts"]),
     ],
     dependencies: [
-       .package(url: "https://github.com/liveview-native/liveview-client-swiftui", branch: "content-builder-live-context")
+       .package(url: "https://github.com/liveview-native/liveview-client-swiftui", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
