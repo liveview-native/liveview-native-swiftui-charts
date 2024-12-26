4bcbebcdffb503c59f3e484393dc4d73adcbb1e7
Carson Katri <Carson.katri@gmail.com>
2024-12-26 12:34:49 -0500

    Depend on client 0.4.0-rc.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["LiveViewNativeCharts"]),
     ],
     dependencies: [
-       .package(url: "https://github.com/liveview-native/liveview-client-swiftui", branch: "main")
+        .package(url: "https://github.com/liveview-native/liveview-client-swiftui", from: "0.4.0-rc.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
